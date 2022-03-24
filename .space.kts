c5d107ef05cb9fe292e9efcec5f7942a42905f14
Mark Rikhter <markrihter98@gmail.com>
2022-03-25 00:09:26 +0700

    Docker ci

--- .space.kts
+++ .space.kts
@@ -5,11 +5,31 @@
 */
 
 job("yandex dev environment") {
-        docker {
-                build {
-                        context = "docker"
-                        args["YANDEX_CLOUD_KEY_ID"] = "YANDEX_CLOUD_KEY_ID_EXAMPLE"
-                        args["YANDEX_CLOUD_SECRET"] = "YANDEX_CLOUD_SECRET_EXAMPLE"
-                }
+    docker {
+        env["YANDEX_CLOUD_KEY_ID"] = Params("yandex_cloud_key_id")
+        env["YANDEX_CLOUD_SECRET"] = Secrets("yandex_cloud_secret")
+
+        build {
+            args["YANDEX_CLOUD_KEY_ID"] = "${'$'}YANDEX_CLOUD_KEY_ID"
+            args["YANDEX_CLOUD_SECRET"] = "${'$'}YANDEX_CLOUD_SECRET"
         }
+    }
+//        container("ubuntu") {
+//                shellScript {
+//                        content = """
+//                                apt update \
+//                                && apt install curl unzip -y \
+//                                && curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh \
+//                                && bash nodesource_setup.sh \
+//                                && apt install nodejs -y \
+//                                && npm i -g yarn \
+//                                && curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+//                                && unzip awscliv2.zip \
+//                                && ./aws/install \
+//                                && yarn install \
+//                                && yarn build \
+//                                && aws configure set aws_access_key_id ${'$'}YANDEX_CLOUD_KEY_ID && aws configure set aws_secret_access_key ${'$'}YANDEX_CLOUD_SECRET && aws configure set default.region ru-central1 \
+//                                && aws s3 --endpoint-url=https://storage.yandexcloud.net cp --recursive ./build s3://fractal-web
+//                        """
+//                }
 }
--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,30 @@
 FROM ubuntu
 
-#WORKDIR /usr/app
+WORKDIR /usr/app
 # install required commands
-#RUN apt update && apt install curl unzip -y
+RUN apt update && apt install curl unzip -y
 
 # install nodejs and yarn
-#RUN curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh \
-#    && bash nodesource_setup.sh \
-#    && apt install nodejs -y \
-#    && npm i -g yarn
+RUN curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh \
+    && bash nodesource_setup.sh \
+    && apt install nodejs -y \
+    && npm i -g yarn
 
 # install aws-cli
-#RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
-#    && unzip awscliv2.zip \
-#    && ./aws/install
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+    && unzip awscliv2.zip \
+    && ./aws/install
 
 # build app
-#COPY package.json .
-#COPY yarn.lock .
-#RUN yarn install
-#COPY . .
-#RUN yarn build
+COPY package.json .
+COPY yarn.lock .
+RUN yarn install
+COPY . .
+RUN yarn build
 
 # load to yandex cloud object storage
 ARG YANDEX_CLOUD_KEY_ID
 ARG YANDEX_CLOUD_SECRET
 
-RUN echo $YANDEX_CLOUD_SECRET && echo $YANDEX_CLOUD_KEY_ID
-
-#RUN aws configure set aws_access_key_id $YANDEX_CLOUD_KEY_ID && aws configure set aws_secret_access_key $YANDEX_CLOUD_SECRET && aws configure set default.region ru-central1
-#CMD aws s3 --endpoint-url=https://storage.yandexcloud.net cp --recursive ./build s3://fractal-web
+RUN aws configure set aws_access_key_id $YANDEX_CLOUD_KEY_ID && aws configure set aws_secret_access_key $YANDEX_CLOUD_SECRET && aws configure set default.region ru-central1
+CMD aws s3 --endpoint-url=https://storage.yandexcloud.net cp --recursive ./build s3://fractal-web
