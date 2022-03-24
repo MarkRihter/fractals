4c61a44d9ad07cadde576d4894bdddda80b15a4d
Mark Rikhter <markrihter98@gmail.com>
2022-03-24 15:26:01 +0000

    Tabulation fix

--- .space.kts
+++ .space.kts
@@ -6,10 +6,10 @@
 
 job("yandex dev environment") {    
         docker {
-        build {
-            context = "docker"
-            args["YANDEX_CLOUD_KEY_ID"] = "YANDEX_CLOUD_KEY_ID_EXAMPLE"
-            args["YANDEX_CLOUD_SECRET"] = "YANDEX_CLOUD_SECRET_EXAMPLE"
-        }
+        	build {
+            	context = "docker"
+            	args["YANDEX_CLOUD_KEY_ID"] = "YANDEX_CLOUD_KEY_ID_EXAMPLE"
+            	args["YANDEX_CLOUD_SECRET"] = "YANDEX_CLOUD_SECRET_EXAMPLE"
+        	}
     }
 }
