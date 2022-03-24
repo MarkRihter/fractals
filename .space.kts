9ab82d766d3043a96288deed60b12d60d5124180
Mark Rikhter <markrihter98@gmail.com>
2022-03-24 23:04:55 +0700

    Docker ci test

--- .space.kts
+++ .space.kts
@@ -4,12 +4,6 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 
-job("yandex dev environment") {    
-        docker {
-        build {
-            context = "docker"
-            args["YANDEX_CLOUD_KEY_ID"] = "YANDEX_CLOUD_KEY_ID_EXAMPLE"
-            args["YANDEX_CLOUD_SECRET"] = "YANDEX_CLOUD_SECRET_EXAMPLE"
-        }
-    }
+job("yandex dev environment") {
+    container(displayName = "Say Hello", image = "hello-world")
 }
