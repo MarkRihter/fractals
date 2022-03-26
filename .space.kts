496dc52008ff6a9ac6bc5b479eeba772a98e3a18
Mark Rikhter <markrihter98@gmail.com>
2022-03-26 23:25:27 +0700

    Docker prod automation added

--- .space.kts
+++ .space.kts
@@ -6,7 +6,6 @@
 
 job("yandex dev environment") {
     docker {
-
         startOn {
             gitPush {
                 branchFilter {
@@ -17,10 +16,30 @@ job("yandex dev environment") {
 
         env["YANDEX_CLOUD_KEY_ID"] = Params("yandex_cloud_key_id")
         env["YANDEX_CLOUD_SECRET"] = Secrets("yandex_cloud_secret")
-
         build {
+            file = "Dockerfile.dev"
             args["YANDEX_CLOUD_KEY_ID"] = "${'$'}YANDEX_CLOUD_KEY_ID"
             args["YANDEX_CLOUD_SECRET"] = "${'$'}YANDEX_CLOUD_SECRET"
+        }
+    }
+
+    docker {
+        startOn {
+            gitPush {
+                branchFilter {
+                    +"refs/heads/main"
+                }
+            }
+        }
+
+        env["YANDEX_COMPUTED_CLOUD_USER"] = Params("yandex_computed_cloud_user")
+        env["YANDEX_COMPUTED_CLOUD_DESTINATION"] = Params("yandex_computed_cloud_destination")
+        env["YANDEX_COMPUTED_CLOUD_PASSWORD"] = Secrets("yandex_computed_cloud_password")
+        build {
+            file = "Dockerfile.prod"
+            args["YANDEX_COMPUTED_CLOUD_USER"] = "${'$'}YANDEX_COMPUTED_CLOUD_USER"
+            args["YANDEX_COMPUTED_CLOUD_DESTINATION"] = "${'$'}YANDEX_COMPUTED_CLOUD_DESTINATION"
+            args["YANDEX_COMPUTED_CLOUD_PASSWORD"] = "${'$'}YANDEX_COMPUTED_CLOUD_PASSWORD"
         }
     }
 }
